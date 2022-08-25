d2ab2ac3623e7d76ce21558132129db42152024b
Dan Trevino <dantrevino@gmail.com>
2022-08-25 10:26:29 -0500

    export to md

--- src/helpers/notes.js
+++ src/helpers/notes.js
@@ -1,45 +1,45 @@
 const FileSaver = require("file-saver");
 
 export function downloadNote(note) {
-  let fileexport = "<html><head></head><body>";
+  let fileexport = "----\n";
   /* eslint-disable no-unused-vars */
   let tagExport = "";
-  let shareExport = "";
-  let i = 0;
-  const self = this;
-  if (!note.headline) {
-    note.headline = "Untitled Note";
+
+  if (!note.title) {
+    note.title = "Untitled Note";
   }
-  fileexport += "title: " + note.title.replace(/#/g, "") + "  </br>";
+  fileexport += "title: " + note.title.replace(/#/g, "") + "\n";
   fileexport += "date:";
   if (note.created) {
     fileexport += " " + new Date(note.created).toISOString();
   }
-  fileexport += "  <br />";
-  fileexport += "created: " + new Date(note.created).toISOString() + " <br />";
+  fileexport += "  \n";
+  fileexport += "created: " + new Date(note.created).toISOString() + " \n ";
   fileexport += "lastmod:";
   if (note.modified) {
     fileexport += " " + new Date(note.modified).toISOString();
   }
-  fileexport += "  <br/>";
-  fileexport += "author: " + note.createdBy + "  <br />";
-  fileexport += "last modified by: " + note.modifiedBy + " <br />";
+  fileexport += "  \n";
+  fileexport += "author: " + note.createdBy + "  \n";
   fileexport += "tags: ";
   if (note.tags.length > 0) {
-    for (i = 0; i < note.tags.length; i++) {
-      tagExport += note.tags[i] + ", ";
+    for (let i = 0; i < note.tags.length; i++) {
+      tagExport += note.tags[i];
+      if (i !== note.tags.length - 1) {
+        tagExport += ", ";
+      }
     }
     tagExport = tagExport.substring(0, tagExport.length - 2);
-    fileexport += tagExport + "  <br/>";
+    fileexport += tagExport + "  \n";
   } else {
-    fileexport += " <br />";
+    fileexport += " \n";
   }
-  fileexport += "favorite: " + note.fave + "  <br />";
-  fileexport += "<br /><p>";
+  fileexport += "favorite: " + note.fave + "  \n";
+  fileexport += "\n\n";
   fileexport += note.text;
-  fileexport += "</p>";
-  fileexport += "</body></html>";
+  fileexport += "\n\n";
+  fileexport += "----\n";
 
   let blob = new Blob([fileexport], { type: "text/plain;charset=utf-8" });
-  FileSaver.saveAs(blob, `${note.title}-${new Date().toISOString()}.html`);
+  FileSaver.saveAs(blob, `${note.title}-${new Date().toISOString()}.md`);
 }
