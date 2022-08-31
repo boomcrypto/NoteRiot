9a4f111b1d0b49809c2478957ceb3bd18ffb8a02
Dan Trevino <dantrevino@gmail.com>
2022-08-31 11:32:43 -0500

    use existing component

--- src/components/FilterBar.vue
+++ src/components/FilterBar.vue
@@ -1,152 +1,118 @@
 <template>
-  <div class="row q-gutter-md justify-center boom-card">
-    <div class="q-pt-sm">Filter By:</div>
-    <q-separator spaced inset vertical dark />
-
-    <div
-      :class="labelFilter !== 'favorite' ? 'bg-accent text-white' : ''"
-      @click="handleFilter('favorite')"
-    >
-      Favorites
-    </div>
+  <q-btn-group outline>
     <q-btn
-      :outline="labelFilter !== 'archive'"
       no-caps
-      square
       color="accent"
-      label="Archives"
-      @click="handleFilter('archive')"
+      label="Favorite"
+      @click="handleFilter('favorite')"
     />
     <q-btn
-      :outline="!allTags.includes(labelFilter)"
       no-caps
-      square
       color="accent"
-      :label="labelChipLabel"
-      :icon-right="labelClick ? 'expand_less' : 'expand_more'"
-      @click="labelClick = !labelClick"
-    >
-      <q-menu>
-        <q-list class="effin-border">
-          <q-item
-            clickable
-            v-close-popup
-            @click="handleFilter(tag)"
-            v-for="tag in allTags"
-            :key="`labelfilter-${tag}`"
-          >
-            <q-item-section>
-              <q-item-label>{{ tag }}</q-item-label>
-            </q-item-section>
-          </q-item>
-        </q-list>
-      </q-menu>
-    </q-btn>
-    <q-btn
-      no-caps
-      :outline="!labelFilter.startsWith('color:')"
-      square
-      label="Colors"
-      :icon-right="colorClick ? 'expand_less' : 'expand_more'"
-      class="q-mr-sm"
-      color="accent"
-      @click="colorClick = !colorClick"
-    >
-      <q-menu>
-        <q-list class="effin-border">
-          <q-item
-            clickable
-            v-close-popup
-            @click="handleFilter(`color:${color}`)"
-            v-for="color in colors"
-            :key="`colorfilter-${color}`"
-          >
-            <q-item-section>
-              <q-item-label>{{ color }}</q-item-label>
-            </q-item-section>
-          </q-item>
-        </q-list>
-      </q-menu>
-    </q-btn>
-    <q-btn
-      no-caps
-      outline
-      rounded
-      color="accent"
-      label="Sort"
-      icon="img:/images/sort.svg"
-    >
-      <q-menu>
-        <q-list class="effin-border">
-          <q-item
-            :active="sortBy === 'createdAt'"
-            active-class="activeSort"
-            clickable
-            v-close-popup
-            v-ripple
-            @click="handleSetCurrentSortBy('createdAt')"
-          >
-            <q-item-section avatar>
-              <q-icon name="access_time" />
-            </q-item-section>
-            <q-item-section> Date</q-item-section>
-            <q-item-section side>
-              <q-icon
-                v-if="sortBy === 'createdAt'"
-                :name="sortDirection === 'asc' ? 'north_east' : 'south_east'"
-                size="14px"
-                class="col"
-              />
-            </q-item-section>
-          </q-item>
-          <q-item
-            :active="sortBy === 'title'"
-            active-class="activeSort"
-            clickable
-            v-ripple
-            v-close-popup
-            @click="handleSetCurrentSortBy('title')"
-          >
-            <q-item-section avatar>
-              <q-icon name="sort_by_alpha" class="col" />
-            </q-item-section>
-            <q-item-section> Title</q-item-section>
-            <q-item-section side>
-              <q-icon
-                v-if="sortBy === 'title'"
-                :name="sortDirection === 'asc' ? 'north_east' : 'south_east'"
-                size="14px"
-                class="col"
-              />
-            </q-item-section>
-          </q-item>
+      label="Archive"
+      @click="handleFilter('archive')"
+    />
+    <q-btn-dropdown no-caps color="accent" auto-close :label="labelChipLabel">
+      <q-list class="effin-border">
+        <q-item
+          clickable
+          v-close-popup
+          @click="handleFilter(tag)"
+          v-for="tag in allTags"
+          :key="`labelfilter-${tag}`"
+        >
+          <q-item-section>
+            <q-item-label>{{ tag }}</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-btn-dropdown>
+    <q-btn-dropdown no-caps color="accent" auto-close label="Colors">
+      <q-list class="effin-border">
+        <q-item
+          clickable
+          v-close-popup
+          @click="handleFilter(`color:${color}`)"
+          v-for="color in colors"
+          :key="`colorfilter-${color}`"
+        >
+          <q-item-section>
+            <q-item-label>{{ color }}</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-btn-dropdown>
+    <q-btn-dropdown no-caps color="accent" label="Sort">
+      <q-list class="effin-border">
+        <q-item
+          :active="sortBy === 'createdAt'"
+          active-class="activeSort"
+          clickable
+          v-close-popup
+          v-ripple
+          @click="handleSetCurrentSortBy('createdAt')"
+        >
+          <q-item-section avatar>
+            <q-icon name="access_time" />
+          </q-item-section>
+          <q-item-section> Date</q-item-section>
+          <q-item-section side>
+            <q-icon
+              v-if="sortBy === 'createdAt'"
+              :name="sortDirection === 'asc' ? 'north_east' : 'south_east'"
+              size="14px"
+              class="col"
+            />
+          </q-item-section>
+        </q-item>
+        <q-item
+          :active="sortBy === 'title'"
+          active-class="activeSort"
+          clickable
+          v-ripple
+          v-close-popup
+          @click="handleSetCurrentSortBy('title')"
+        >
+          <q-item-section avatar>
+            <q-icon name="sort_by_alpha" class="col" />
+          </q-item-section>
+          <q-item-section> Title</q-item-section>
+          <q-item-section side>
+            <q-icon
+              v-if="sortBy === 'title'"
+              :name="sortDirection === 'asc' ? 'north_east' : 'south_east'"
+              size="14px"
+              class="col"
+            />
+          </q-item-section>
+        </q-item>
 
-          <q-item
-            :active="sortBy === 'updatedAt'"
-            active-class="activeSort"
-            clickable
-            v-ripple
-            v-close-popup
-            @click="handleSetCurrentSortBy('updatedAt')"
-          >
-            <q-item-section avatar>
-              <q-icon name="update" />
-            </q-item-section>
-            <q-item-section> Updated</q-item-section>
-            <q-item-section side>
-              <q-icon
-                v-if="sortBy === 'updatedAt'"
-                :name="sortDirection === 'asc' ? 'north_east' : 'south_east'"
-                size="14px"
-                class="col"
-              />
-            </q-item-section>
-          </q-item>
-        </q-list>
-      </q-menu>
-    </q-btn>
+        <q-item
+          :active="sortBy === 'updatedAt'"
+          active-class="activeSort"
+          clickable
+          v-ripple
+          v-close-popup
+          @click="handleSetCurrentSortBy('updatedAt')"
+        >
+          <q-item-section avatar>
+            <q-icon name="update" />
+          </q-item-section>
+          <q-item-section> Updated</q-item-section>
+          <q-item-section side>
+            <q-icon
+              v-if="sortBy === 'updatedAt'"
+              :name="sortDirection === 'asc' ? 'north_east' : 'south_east'"
+              size="14px"
+              class="col"
+            />
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-btn-dropdown>
     <q-btn
       round
+      size="sm"
       unelevated
       outlined
       no-caps
@@ -158,7 +124,7 @@
       @click="handleClearFilter"
     >
     </q-btn>
-  </div>
+  </q-btn-group>
 </template>
 
 <script>
